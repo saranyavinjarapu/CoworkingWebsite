609072fca80db2ca0f9477b92ad838c4715e9789
saranya vinjarapu <68265309+saranyavinjarapu@users.noreply.github.com>
2023-01-12 11:34:52 +0530

    added sass/scss to the project

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import Head from "next/head";
+import styles from "../styles/Home.module.scss";
 
 export default function Home() {
   return (
-    <Head>
-      <title>Planet Plus Coworking Space</title>
-      <meta name="description" content="Planet Plus Coworking Space by ICD" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-    </Head>
+    <>
+      <Head>
+        <title>Planet Plus Coworking Space</title>
+        <meta name="description" content="Planet Plus Coworking Space by ICD" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <h1 className={styles.main}>hello true</h1>
+    </>
   );
 }
