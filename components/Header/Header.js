b5850fa42af1d6f3ec5c64909a5700ab072bbd31
saranya vinjarapu <68265309+saranyavinjarapu@users.noreply.github.com>
2023-01-14 12:17:52 +0530

    refined code to make header a separate component by itself

---
+++ components/Header/Header.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Image from "next/image";
+import planetPlusRectangle from "../../public/planetPlusRectangle.svg";
+import linkArrow from "../../public/linkArrow.svg";
+import styles from "./Header.module.scss";
+
+const Header = () => {
+  return (
+    <div>
+      <div className={styles.headerContainer}>
+        <div className={styles.title}>Planet Plus</div>
+        <div className={styles.description}>
+          Co-working, caregiver spaces and a cafe. Be productive; make friends;
+          become a part of the Healthy Planet community.
+          <div className={styles.descriptionPlus}>
+            <Image
+              src={planetPlusRectangle}
+              alt="planet plus icon"
+              width="350px"
+              height="300px"
+              className={styles.planetPlusRectangle}
+            />
+            PLANET PLUS
+            <Image
+              src={linkArrow}
+              alt="link arrow to planet plus"
+              width="350px"
+              height="300px"
+              className={styles.linkArrow}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- components/PlanetPlusContainer/PlanetPlusContainer.js
+++ components/PlanetPlusContainer/PlanetPlusContainer.js
@@ -1,37 +1,12 @@
 import styles from "./PlanetPlusContainer.module.scss";
 import FeatureInfoCard from "../FeatureInfoCard/FeatureInfoCard";
-import Image from "next/image";
-import planetPlusRectangle from "../../public/planetPlusRectangle.svg";
-import linkArrow from "../../public/linkArrow.svg";
+import Header from "../Header/Header";
 import featuresData from "../../data/featuresData";
 
 const PlanetPlusContainer = () => {
   return (
     <div className={styles.gridContainer}>
-      <div className={styles.titleContainer}>
-        <div className={styles.title}>Planet Plus</div>
-        <div className={styles.description}>
-          Co-working, caregiver spaces and a cafe. Be productive; make friends;
-          become a part of the Healthy Planet community.
-          <div className={styles.descriptionPlus}>
-            <Image
-              src={planetPlusRectangle}
-              alt="planet plus icon"
-              width="350px"
-              height="300px"
-              className={styles.planetPlusRectangle}
-            />
-            PLANET PLUS
-            <Image
-              src={linkArrow}
-              alt="link arrow to planet plus"
-              width="350px"
-              height="300px"
-              className={styles.linkArrow}
-            />
-          </div>
-        </div>
-      </div>
+      <Header></Header>
       <ul className={`${styles.scrollableList} ${styles.full}`}>
         {featuresData.map((row, index) => (
           <FeatureInfoCard
